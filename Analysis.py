fcef9594c117efb88392b28b519d6339f7337576
Aadith Yadav <aadith.ygalu@gmail.com>
2023-05-13 16:01:26 +0200

    Added castling

--- Analysis.py
+++ Analysis.py
@@ -8,13 +8,39 @@ class Analysis:
         self.move_made = False
         self.prev_move = None
 
-        # TODO: Castling
+        # TODO: Castling into check
         self.b_rook1_moved = False
         self.w_rook1_moved = False
         self.b_rook2_moved = False
         self.w_rook2_moved = False
         self.b_king_moved = False
         self.w_king_moved = False
+
+        self.evaluation = 0
+        self.evaluate()
+
+    def evaluate(self):
+        self.evaluation = 0
+        for i in self.board:
+            for j in i:
+                if j is not None:
+                    if j == 0:
+                        self.evaluation -= 1
+                    elif j == 1:
+                        self.evaluation -= 5
+                    elif j == 2 or j == 3:
+                        self.evaluation -= 3
+                    elif j == 5:
+                        self.evaluation -= 9
+                    elif j == 6:
+                        self.evaluation += 1
+                    elif j == 7:
+                        self.evaluation += 5
+                    elif j == 8 or j == 9:
+                        self.evaluation += 3
+                    elif j == 11:
+                        self.evaluation += 9
+
 
     def legal_moves(self, i, j):
         moves = []
@@ -445,9 +471,16 @@ class Analysis:
                 else:
                     moves.append((i - k - 1, j + k + 1))
 
-        # TODO: King movements: Check, Checkmate, Castle, Run into checks, In front of king
+        # TODO: King movements: Check, Checkmate, Castle into checks, Run into checks, In front of king
         if self.board[i][j] == 4:
             # black king
+            if not self.b_king_moved and not self.b_rook1_moved:
+                if self.board[0][5] is None and self.board[0][6] is None:
+                    moves.append((0, 7, "b0-0"))
+            if not self.b_king_moved and not self.b_rook2_moved:
+                if self.board[0][3] is None and self.board[0][2] is None and self.board[0][1] is None:
+                    moves.append((0, 0, "b0-0-0"))
+
             if i + 1 <= 7:
                 if self.board[i + 1][j] is None:
                     moves.append((i + 1, j))
@@ -504,10 +537,15 @@ class Analysis:
                     if self.board[i - 1][j + 1] >= 6:
                         moves.append((i - 1, j + 1))
 
-
-
         if self.board[i][j] == 10:
             # white king
+            if not self.w_king_moved and not self.w_rook1_moved:
+                if self.board[7][5] is None and self.board[7][6] is None:
+                    moves.append((7, 7, "w0-0"))
+            if not self.w_king_moved and not self.w_rook2_moved:
+                if self.board[7][3] is None and self.board[7][2] is None and self.board[7][1] is None:
+                    moves.append((7, 0, "w0-0-0"))
+
             if i + 1 <= 7:
                 if self.board[i + 1][j] is None:
                     moves.append((i + 1, j))
@@ -679,8 +717,19 @@ class Analysis:
         return moves
 
     def move(self, i0, j0, i1, j1):
+        legal_moves = self.legal_moves(i0, j0)
+        if (i1, j1) in legal_moves:
+            if self.board[i1][j1] == 1:
+                if i1 == 0 and j1 == 0:
+                    self.b_rook2_moved = True
+                elif i1 == 0 and j1 == 7:
+                    self.b_rook1_moved = True
+            if self.board[i1][j1] == 7:
+                if i1 == 7 and j1 == 0:
+                    self.w_rook2_moved = True
+                elif i1 == 7 and j1 == 7:
+                    self.w_rook1_moved = True
 
-        if (i1, j1) in self.legal_moves(i0, j0):
             temp = self.board[i0][j0]
             self.board[i1][j1] = temp
             self.board[i0][j0] = None
@@ -689,10 +738,88 @@ class Analysis:
             if self.board[i1][j1] == 6 and i1 == 0:
                 self.board[i1][j1] = 11
 
+            if self.board[i0][j0] == 1:
+                if i0 == 0 and j0 == 0:
+                    if not self.b_rook1_moved:
+                        self.b_rook1_moved = True
+
+                if i0 == 0 and j0 == 7:
+                    if not self.b_rook2_moved:
+                        self.b_rook2_moved = True
+
+            if self.board[i0][j0] == 7:
+                if i0 == 7 and j0 == 0:
+                    if not self.w_rook1_moved:
+                        self.w_rook1_moved = True
+
+                if i0 == 7 and j0 == 7:
+                    if not self.w_rook2_moved:
+                        self.w_rook2_moved = True
+
+            if self.board[i0][j0] == 4:
+                if i0 == 0 and j0 == 4:
+                    if not self.b_king_moved:
+                        self.b_king_moved = True
+
+            if self.board[i0][j0] == 10:
+                if i0 == 7 and j0 == 4:
+                    if not self.w_king_moved:
+                        self.w_king_moved = True
+
             self.prev_move = (i0, j0, i1, j1)
             self.move_made = True
 
-        elif (i1, j1, "epr") in self.legal_moves(i0, j0):
+        elif (i1, j1, "w0-0") in legal_moves:
+            self.board[7][4] = None
+            self.board[7][5] = 7
+            self.board[7][6] = 10
+            self.board[7][7] = None
+
+            self.w_rook2_moved = True
+            self.w_king_moved = True
+
+            self.prev_move = (i0, j0, i1, j1)
+            self.move_made = True
+
+        elif (i1, j1, "w0-0-0") in legal_moves:
+            self.board[7][4] = None
+            self.board[7][3] = 7
+            self.board[7][2] = 10
+            self.board[7][1] = None
+            self.board[7][0] = None
+
+            self.w_rook1_moved = True
+            self.w_king_moved = True
+
+            self.prev_move = (i0, j0, i1, j1)
+            self.move_made = True
+
+        elif (i1, j1, "b0-0") in legal_moves:
+            self.board[0][4] = None
+            self.board[0][5] = 1
+            self.board[0][6] = 4
+            self.board[0][7] = None
+
+            self.b_rook2_moved = True
+            self.b_king_moved = True
+
+            self.prev_move = (i0, j0, i1, j1)
+            self.move_made = True
+
+        elif (i1, j1, "b0-0-0") in legal_moves:
+            self.board[0][4] = None
+            self.board[0][3] = 1
+            self.board[0][2] = 4
+            self.board[0][1] = None
+            self.board[0][0] = None
+
+            self.b_rook2_moved = True
+            self.b_king_moved = True
+
+            self.prev_move = (i0, j0, i1, j1)
+            self.move_made = True
+
+        elif (i1, j1, "epr") in legal_moves:
             temp = self.board[i0][j0]
 
             self.board[i0][j0 + 1] = None
@@ -707,7 +834,7 @@ class Analysis:
             self.prev_move = (i0, j0, i1, j1)
             self.move_made = True
 
-        elif (i1, j1, "epl") in self.legal_moves(i0, j0):
+        elif (i1, j1, "epl") in legal_moves:
             temp = self.board[i0][j0]
 
             self.board[i0][j0 - 1] = None
@@ -729,5 +856,7 @@ class Analysis:
                 self.turn = "b"
             else:
                 self.turn = "w"
+
+        self.evaluate()
 
         return self.board

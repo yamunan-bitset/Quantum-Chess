1c2a07131b26a9fab4c4cf86ed0d77d87d3ff452
Aadith Yadav <aadith.ygalu@gmail.com>
2023-05-23 17:15:26 +0200

    Fixed test

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
 from Analysis import Analysis
 from Pieces import Pieces
 from copy import deepcopy
+from Board import Board
+import os
+import pygame
 
-engine = Analysis(Pieces.load_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+pygame.init()
+
+logo = pygame.image.load(os.path.join("texture", "black", "knight.png"))
+pygame.display.set_icon(logo)
+pygame.display.set_caption("Chess Analysis")
+screen = pygame.display.set_mode((1000, 800))
+screen.fill((36, 34, 30))
+
+board = Board(screen)
+
+startpos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+pieces = Pieces(screen, startpos)
 
 actual_nodes = [1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860, 84998978956, 2439530234167, 69352859712417]
-def depth_test(n, board, log=False):
+
+def depth_test(n, mboard, turn, log=False):
     if n == 0:
         return 1
 
@@ -15,19 +30,27 @@ def depth_test(n, board, log=False):
     legal_moves = []
     for x in range(8):
         for y in range(8):
-            if engine.board[x][y] is not None:
-                moves = engine.legal_moves(x, y)
-                if moves == []:
-                    continue
-                for i in moves:
-                    legal_moves.append((x, y, i))
+            if pieces.analysis.board[x][y] is not None:
+                if (turn == "w" and pieces.analysis.board[x][y] >= 6) or (turn == "b" and pieces.analysis.board[x][y] < 6):
+                    moves = pieces.analysis.legal_moves(x, y, ignore_turn=True)
+                    if moves == []:
+                        continue
+                    for i in moves:
+                        legal_moves.append((x, y, i))
 
     nodes = 0
     for move in legal_moves:
-        _board = deepcopy(board)
-        board = engine.depth(*move, board=board)
-        nodes += depth_test(n - 1, board)
-        board = deepcopy(_board)
+        _board = deepcopy(mboard)
+        mboard = pieces.analysis.depth(*move, board=mboard)
+        board.board = mboard
+        pieces.board = mboard
+        board.render(pieces.analysis)
+        pieces.render()
+        pygame.display.update()
+        nodes += depth_test(n - 1, mboard, "b" if turn == "w" else "w")
+        mboard = _board
+        board.board = board
+        pieces.board = board
 
     if log:
         print(f"{nodes=}")
@@ -35,10 +58,6 @@ def depth_test(n, board, log=False):
     return nodes
 
 
-if __name__ == "__main__":
-    print()
-    depth_test(1, engine.board, log=True)
-    depth_test(2, engine.board, log=True)
-    depth_test(3, engine.board, log=True)
-    depth_test(4, engine.board, log=True)
-    depth_test(5, engine.board, log=True)
+screen.fill((36, 34, 30))
+
+depth_test(2, Pieces.load_fen(startpos), "w", log=True)

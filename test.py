56c3ecc8ff235b5f373ad78e6e6cd1014459a37b
Aadith Yadav <aadith.ygalu@gmail.com>
2023-05-23 18:40:21 +0200

    Added pos5 test

--- test.py
+++ test.py
@@ -16,13 +16,20 @@ screen.fill((36, 34, 30))
 board = Board(screen)
 
 startpos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-pieces = Pieces(screen, startpos)
+pos5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
+pieces = Pieces(screen, pos5)
 
-actual_nodes = [1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860, 84998978956, 2439530234167, 69352859712417]
+startpos_actual_nodes = [1, 20, 400, 8902, 197281, 4865609, 119060324, 3195901860, 84998978956, 2439530234167, 69352859712417]
+# Startpos test:
 # depth 1, test node 20
 # depth 2, test node 400
 # depth 3, test node 8902
 # depth 4, test node 197281
+
+pos5_actual_nodes = [1, 44, 1486, 62379, 2103487, 89941194]
+# Position 5 test:
+# depth 1, test node 41
+# depth 2, test node 1373
 
 def depth_test(n, mboard, turn, log=False):
     if n == 0:
@@ -54,7 +61,7 @@ def depth_test(n, mboard, turn, log=False):
         board.render(pieces.analysis)
         pieces.render()
         pygame.display.update()
-        # pygame.event.wait()
+        #pygame.time.wait(10)
         nodes += depth_test(n - 1, mboard, "b" if turn == "w" else "w")
         mboard = _board
         board.board = _board
@@ -69,4 +76,4 @@ def depth_test(n, mboard, turn, log=False):
 
 screen.fill((36, 34, 30))
 
-depth_test(4, Pieces.load_fen(startpos), "w", log=True)
+depth_test(2, Pieces.load_fen(pos5), "w", log=True)

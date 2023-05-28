6be2c84c1402a7c1c1dcdb4b671806342aa79d9b
Aadith Yadav <aadith.ygalu@gmail.com>
2023-05-28 10:23:34 +0200

    Added multiple connections

--- server.py
+++ server.py
@@ -16,25 +16,29 @@ turn = "w"
 def recv_send(turn):
     while True:
         if turn == "w":
-            try:
-                move = clients[0].recv(1024)
-                recv = pickle.loads(move)
-                print(f"Received {recv=}")
-                clients[1].send(move)
-                print(f"Sent {recv=}")
-                turn = "b"
-            except:
-                continue
+            if len(clients) % 2 == 0:
+                for i in range(0, len(clients), 2):
+                    try:
+                        move = clients[i].recv(1024)
+                        recv = pickle.loads(move)
+                        print(f"Received {recv=}")
+                        clients[i + 1].send(move)
+                        print(f"Sent {recv=}")
+                        turn = "b"
+                    except:
+                        continue
         else:
-            try:
-                move = clients[1].recv(1024)
-                recv = pickle.loads(move)
-                print(f"Received {recv=}")
-                clients[0].send(move)
-                print(f"Sent {recv=}")
-                turn = "w"
-            except:
-                continue
+            if len(clients) % 2 == 0:
+                for i in range(0, len(clients), 2):
+                    try:
+                        move = clients[i + 1].recv(1024)
+                        recv = pickle.loads(move)
+                        print(f"Received {recv=}")
+                        clients[i].send(move)
+                        print(f"Sent {recv=}")
+                        turn = "w"
+                    except:
+                        continue
 
 
 def connect():

266cbb97d8dce668b1f43ec921ef7842a214f4d1
Aadith Yadav <aadith.ygalu@gmail.com>
2024-01-30 21:42:13 +0100

    Fixed dir

--- Perft_test/test.py
+++ Perft_test/test.py
@@ -27,10 +27,11 @@ pos5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
 pos6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
 
 #######
-DEPTH = 4
-POS = pos6
+DEPTH = 2
+POS = startpos
 #######
 
+os.chdir("..")
 pieces = Pieces(screen, POS)
 
 
